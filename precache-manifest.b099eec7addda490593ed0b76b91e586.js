fc1b8cae05e33d7a1de81d7bc138d7af4b91d5a6
Victor Biletskiy <victorbiletskiy82@gmail.com>
2019-08-23 13:24:00 +0300

    Updates

---
+++ precache-manifest.b099eec7addda490593ed0b76b91e586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca25565724cc368c9f09ea62fc70734",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "5ce117cf2f6391a15206",
+    "url": "/star-db/static/css/main.7d6cf392.chunk.css"
+  },
+  {
+    "revision": "215aa2aa8b736e62714c",
+    "url": "/star-db/static/js/2.1084bb38.chunk.js"
+  },
+  {
+    "revision": "5ce117cf2f6391a15206",
+    "url": "/star-db/static/js/main.fdc95e9a.chunk.js"
+  },
+  {
+    "revision": "e289c39d995ecb58a7ee",
+    "url": "/star-db/static/js/runtime~main.a96cccf8.js"
+  }
+]);
